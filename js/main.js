a5261f00c76b278329a87559dc908a0c523ba1a7
Ilian Stefanov <markuckataa@gmail.com>
2017-10-19 15:33:06 +0300

    adding animated buttons

--- js/main.js
+++ js/main.js
@@ -18,7 +18,20 @@
         $projectSubtitle = $project.find('.project-subtitle'),
         $projectImage = $project.find('.project-image');
     
-        console.log($pixel);
+        // ProjectCTA (Call to Action)
+        var tlProjectsCTA = new TimelineMax();
+    
+        var $projectLink = $project.find('.button-container'),
+            $projectLinkButton = $project.find('.button'),
+            $projectLinkSpan = $project.find('.bp'),
+            $projectLinkText = $project.find('.bp-text');
+        
+    
+        
+        tlProjectsCTA
+	        .staggerFrom($projectLinkSpan, 0.3, {autoAlpha: 0, yPercent: -100, ease:Back.easeOut}, 0.1)
+	    	.from($projectLinkText,  0.3, {autoAlpha: 0, x: '-100%', ease:Power4.easeInOut}, '-=0.1');
+    
         // move image from the far left
         tlProject
             .set([$projectTitle, $projectSubtitle, $pixel], {autoAlpha: 0})
@@ -29,6 +42,7 @@
             .fromTo($projectTitle, 0.7, {autoAlpha: 0, xPercent: '-50'}, {autoAlpha: 1, xPercent: '-5', ease:Power4.easeInOut}, '-=0.4')
             .fromTo($projectSubtitle, 0.7, {autoAlpha: 0, xPercent: '-50'}, {autoAlpha: 1, xPercent: '-2', ease:Power4.easeInOut}, '-=0.5')
             .add('titleIn')
+            .add(tlProjectsCTA, '+=2')
             .to($projectTitle, 4, {xPercent: '+=5', ease:Linear.easeNone}, 'titleIn-=0.1' )
             .to($projectSubtitle, 4, {xPercent: '+=2', ease:Linear.easeNone}, 'titleIn-=0.2')
             .add('titleOut')
@@ -37,8 +51,7 @@
             .to($pixels, 4.1, {xPercent: '-5', ease:Linear.easeNone}, 'pixelsIn')
             .to([$projectTitle, $projectSubtitle], 0.4, {autoAlpha: 0, xPercent: '+=10', ease:Power4.easeInOut}, 'titleOut')
             .to($projectImage, 0.5, {autoAlpha: 0, xPercent: '+=80', ease:Power4.easeInOut}, 'imageOut');
-            
-})(jQuery);
+    })(jQuery);
 
 
 
